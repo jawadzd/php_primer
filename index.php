c5960ecc847a0ed23f4ed0f3f0fba227c7c2fb7b
Jawad Zeyneddine <jawad.zein.deen@hotmail.com>
2022-03-18 21:50:47 +0200

    Update index.php
    i added 2 more buttons

--- index.php
+++ index.php
@@ -30,6 +30,8 @@ include 'includes/header.php';
     ?>
 
 <button type="button" class="btn btn-primary">CLICK ME!</button>
+<button type="button" class="btn btn-dark">CLICK ME!</button>
+<button type="button" class="btn btn-success">CLICK ME!</button>
 
 
     <?php
